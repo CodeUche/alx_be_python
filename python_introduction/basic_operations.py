d6558c5cd114d00fbed33b6548bfe708613997d2
Code-Uche <ezeprecious.uche@gmail.com>
2025-05-22 14:36:41 +0100

    AlxBE python basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10 # The value 10 is assigned to the variable number1
+number2 = 5 # The value 5 is assigned to the variable number2
+
+subtraction_result = number1 - number2 # This sums up the value of number1 minus number2
+print (subtraction_result) # This prints the result of the variable named subtraction_result
+
+addition_result = number1 + number2 # This adds the value of number1 and number2
+print (addition_result) # This prints the result of the addition of number1 and number2
+
+multiplication_result = number1 * number2 # This multiplies number1 with number2
+print (multiplication_result) # This prints the value of number1 multiplied by number2
+
+
