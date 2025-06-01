c7a8ef3e5f6b62bff33112a9fa5af020deda18d0
Code-Uche <ezeprecious.uche@gmail.com>
2025-06-01 21:08:35 +0100

    Weather advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,13 +2,14 @@
 # Weather report and the right clothing for each weather condition
 
 weather_report = input("What's the weather like roday? (sunny/rainy/cold): ")
+
 if weather_report == "sunny":
         print("Wear a t-shirt and sunglasses.")
 
 elif weather_report == "rainy":
         print("Don't forget your umbrella and a raincoat.")
 
-elif waether_report == "cold" :
+elif weather_report == "cold" :
         print("Make sure to wear a warm coat and a scarf.")
 
 else:
