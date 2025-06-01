2a1c7425d14e0e08bb9e6b0cd2c5831e4221e091
Code-Uche <ezeprecious.uche@gmail.com>
2025-06-01 22:53:39 +0100

    Match case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,21 +7,21 @@ operation = input("Enter an operator (+, -, *, /): ")
 match operation:
     case "+" :
         result = num1 + num2
-            print("The result is: ", result)
+        print("The result is: ", result)
     case "-":
         result = num1 - num2
-            print("The result is: ", result)
+        print("The result is: ", result)
 
     case "*":
         result = num1 * num2
-            print("The result is: ", result)
+        print("The result is: ", result)
 
     case "/":
         if num2 == 0:
             print("Cannot be divided by 0")
         else:
             result = num1 / num2
-                print("The result is ", result)
+            print("The result is ", result)
     case _:
         print("This is an invalid entry")
 
